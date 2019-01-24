e0d0f4e0030f9ee23940c7d3576366a42174b2a5
Clouds <34452445+yhb1206@users.noreply.github.com>
2019-01-24 23:56:37 +0800

    更改

--- LIB/YhbLIB/inc/Yhb_SysTick.h
+++ LIB/YhbLIB/inc/Yhb_SysTick.h
@@ -3,8 +3,8 @@
 
 #include "stm32f10x.h"
 
-void Yhb_SysTick_Init(void);
-void Yhb_Delay_Ms( __IO uint32_t ms);
+extern void Yhb_SysTick_Init(void);
+extern void Yhb_Delay_Ms( __IO uint32_t ms);
 
 
 #endif /* __SYSTICK_H */
